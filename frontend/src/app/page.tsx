0db4fe2e8238f1a8773d014e3132ff08101bbe6d
NaNu831 <>
2025-10-30 22:09:59 +0100

    Moved files into new folder structure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from './page.module.css';
 import Container from "@mui/material/Container";
 import StartPage from "@/app/start/page";
+import Chatbox from "@/app/components/Chatbox";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
           <main className={styles['main']}>
               <Container style={{ backgroundColor: "white"}}>
                 <StartPage/>
+                  <Chatbox/>
               </Container>
           </main>
       </div>
